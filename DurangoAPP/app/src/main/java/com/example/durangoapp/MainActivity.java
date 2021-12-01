ad1f990a74087f2895e34b78159dcbb5d6a8ecca
mario <marioiram2000@gmail.com>
2021-12-01 12:00:45 +0100

    Inicio
    Creados xml del main activity, crear nuevo grupo y seleccioanr grupo

--- DurangoAPP/app/src/main/java/com/example/durangoapp/MainActivity.java
+++ DurangoAPP/app/src/main/java/com/example/durangoapp/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.durangoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void newGame(View view){
+        crearGrupo();
+    }
+
+    public void resumeGame(View view){
+        elegirGrupo();
+    }
+
+    private void crearGrupo(){
+        Dialog dialogCrearGrupo = new Dialog(this);
+        dialogCrearGrupo.setContentView(R.layout.dialog_crear_grupo);
+
+        Button startBtn = (Button) dialogCrearGrupo.findViewById(R.id.dialogCrearGrupoButtonStart);
+        EditText etGroup = dialogCrearGrupo.findViewById(R.id.dialogCrearGrupoEditTextGroup);
+
+        dialogCrearGrupo.show();
+
+
+        startBtn.setOnClickListener(view -> {
+            String group = etGroup.getText().toString();
+            if(group.equals("")){
+                etGroup.setError(getString(R.string.new_group_error));
+            }else{
+                etGroup.setError(null );
+            }
+        });
+    }
+
+    private void elegirGrupo(){
+        Dialog dialogElegirGrupo = new Dialog(this);
+        dialogElegirGrupo.setContentView(R.layout.dialog_elegir_grupo);
+
+        //Button startBtn = (Button) dialogElegirGrupo.findViewById(R.id.dialogCrearGrupoButtonStart);
+        //EditText etGroup = dialogElegirGrupo.findViewById(R.id.dialogCrearGrupoEditTextGroup);
+
+        dialogElegirGrupo.show();
+
     }
 }
---
+++ DurangoAPP/app/src/main/java/com/example/durangoapp/zona0_1.java
@@ -0,0 +1,13 @@
+package com.example.durangoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class zona0_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zona0_1);
+    }
+}
