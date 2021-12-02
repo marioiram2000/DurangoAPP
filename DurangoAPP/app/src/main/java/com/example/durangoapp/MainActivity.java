5346f427a045bf11f5853aa56c006aa23493814a
Ander Rodriguez <anderrod01@gmail.com>
2021-12-02 11:58:22 +0100

    estilos , fuente y temas

--- DurangoAPP/app/src/main/java/com/example/durangoapp/MainActivity.java
+++ DurangoAPP/app/src/main/java/com/example/durangoapp/MainActivity.java
@@ -16,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
     public void newGame(View view){
