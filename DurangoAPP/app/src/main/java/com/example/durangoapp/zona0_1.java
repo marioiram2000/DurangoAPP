b6c3f009024a103e588860e574a10eeb9c81549a
Airam123456 <perseo_39@hotmail.es>
2021-12-09 08:12:11 +0100

    continuacion

--- DurangoAPP/app/src/main/java/com/example/durangoapp/zona0_1.java
+++ DurangoAPP/app/src/main/java/com/example/durangoapp/zona0_1.java
@@ -2,12 +2,28 @@ package com.example.durangoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class zona0_1 extends AppCompatActivity {
+
+    private ImageView imgZona0_1_Principal, imgZona0_1_Txorimalo;
+    private TextView txtZona0_1_Txorimalo, txtZona0_1_Narrador;
+    private Button btnZona0_1_Start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zona0_1);
+
+        imgZona0_1_Principal = findViewById(R.id.imgZona0_1_Principal);
+        imgZona0_1_Txorimalo = findViewById(R.id.imgZona0_1_Txorimalo);
+        txtZona0_1_Narrador = findViewById(R.id.txtZona0_1_Narrador);
+        txtZona0_1_Txorimalo = findViewById(R.id.txtZona0_1_Txorimalo);
+        btnZona0_1_Start = findViewById(R.id.btnZona0_1_Start);
+
+
+
     }
 }
