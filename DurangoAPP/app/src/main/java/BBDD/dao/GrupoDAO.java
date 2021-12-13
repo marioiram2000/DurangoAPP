a578bfdf9d609ed947cc30fe5ebe683f9ca7bfe8
mario <marioiram2000@gmail.com>
2021-12-13 12:00:42 +0100

    Insert grupo

--- DurangoAPP/app/src/main/java/BBDD/dao/GrupoDAO.java
+++ DurangoAPP/app/src/main/java/BBDD/dao/GrupoDAO.java
@@ -1,4 +1,17 @@
 package BBDD.dao;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+import BBDD.modelo.Grupo;
+
 public class GrupoDAO {
+
+    public Grupo insert(SQLiteDatabase db, String nombre){
+        ContentValues nuevoGrupo = new ContentValues();
+        nuevoGrupo.put("nombre", nombre);
+        long id = db.insert("grupos", null, nuevoGrupo);
+        return new Grupo((int)id, nombre);
+    }
+    
 }
