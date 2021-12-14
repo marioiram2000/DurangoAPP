a3a0f01fd664033082e65b34293b61cfa17bcd6f
Airam123456 <perseo_39@hotmail.es>
2021-12-14 08:05:32 +0100

    zona 0

--- DurangoAPP/app/src/main/java/com/example/durangoapp/zona0_1.java
+++ DurangoAPP/app/src/main/java/com/example/durangoapp/zona0_1.java
@@ -1,6 +1,8 @@
 package com.example.durangoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -20,6 +22,7 @@ public class zona0_1 extends AppCompatActivity {
     private ImageView imgZona0_1_Principal, imgZona0_1_Txorimalo,imgZona0_1_Sobre;
     private TextView txtZona0_1_Txorimalo, txtZona0_1_Narrador;
     private Button btnZona0_1_Start, btnZona0_1_Skip;
+    private MediaPlayer audio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +52,11 @@ public class zona0_1 extends AppCompatActivity {
             }
         });
 
+<<<<<<< Updated upstream
+=======
+        audio = MediaPlayer.create(zona0_1.this,R.raw.zona0_1_narrador);
+        audio.start();
+>>>>>>> Stashed changes
 
         btnZona0_1_Skip.setOnClickListener(new View.OnClickListener() {
             @Override
