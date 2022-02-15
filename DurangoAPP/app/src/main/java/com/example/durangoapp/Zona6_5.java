486e7aa6aff7c9b155e5d595a559bae5d2bc7d87
mario <marioiram2000@gmail.com>
2022-02-15 11:46:33 +0100

    juego 6

---
+++ DurangoAPP/app/src/main/java/com/example/durangoapp/Zona6_5.java
@@ -0,0 +1,14 @@
+package com.example.durangoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Zona6_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zona65);
+    }
+}
